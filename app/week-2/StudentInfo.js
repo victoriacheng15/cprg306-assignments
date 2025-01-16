d54c3b3ccb9100dcca92de6023f9dfc60536fe05
Victoria Cheng <victoria.cheng15@gmail.com>
2025-01-16 13:19:59 -0700

    feat: add student info component

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Victoria Cheng</h1>
+      <Link href="https://github.com/victoriacheng15/cprg306-assignments">
+        https://github.com/victoriacheng15/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
