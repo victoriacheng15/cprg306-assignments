6932e200b451d53d710e5e559c2b353557976673
Victoria Cheng <victoria.cheng15@gmail.com>
2025-01-22 18:51:05 -0700

    chore: add style to week 2 page (#4)

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -4,7 +4,10 @@ export default function StudentInfo() {
   return (
     <div>
       <h1>Victoria Cheng</h1>
-      <Link href="https://github.com/victoriacheng15/cprg306-assignments">
+      <Link
+        href="https://github.com/victoriacheng15/cprg306-assignments"
+        className="hover:underline duration-300 ease-in-out"
+      >
         https://github.com/victoriacheng15/cprg306-assignments
       </Link>
     </div>
