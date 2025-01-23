2564f09a4e710ae757f3cbf4b4d4eba0a0ff1786
Victoria Cheng <victoria.cheng15@gmail.com>
2025-01-23 01:52:08 +0000

    feat: add week 3 assignment

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <h2 className="text-3xl font-bold m-2">Shopping List</h2>
+      <ItemList />
+    </main>
+  );
+}
