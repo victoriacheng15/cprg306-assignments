4809422000858be7ae697449dbfa2f40369f0c3f
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-26 19:55:48 -0700

    add style for shopping link and a mmessage for access the link w/o login

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,10 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { useUserAuth } from "./_utils/auth-context";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowMessage(true);
+  };
 
   const handleSignIn = async () => {
     try {
@@ -29,7 +36,7 @@ export default function Page() {
           <p className="mb-4">
             Welcome, {user.displayName} ({user.email})
           </p>
-          <Link href="/week-9/shopping-list">Go to shopping list</Link>
+          <ShoppingLink />
           <button
             onClick={handleSignOut}
             className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
@@ -39,7 +46,12 @@ export default function Page() {
         </div>
       ) : (
         <div className="grid gap-4">
-          <Link href="/week-9/shopping-list">Go to shopping list</Link>
+          <ShoppingLink onClick={handleClick} />
+          {showMessage && (
+            <p className="text-red-400 mt-2">
+              Please log in to access the shopping list.
+            </p>
+          )}
           <button
             onClick={handleSignIn}
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
@@ -49,5 +61,17 @@ export default function Page() {
         </div>
       )}
     </div>
+  );
+}
+
+function ShoppingLink({ onClick }) {
+  return (
+    <Link
+      className="text-blue-500 text-lg hover:text-blue-700 transition-colors duration-200"
+      href="/week-9/shopping-list"
+      onClick={onClick}
+    >
+      Go to shopping list
+    </Link>
   );
 }
